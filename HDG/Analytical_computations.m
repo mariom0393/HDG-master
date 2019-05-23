8000a0d26579ae36bb5d5b3ae8def4acecb84a8a
Eugenio <ej.muttiozavala@ugto.mx>
2019-05-23 14:09:47 +0200

    robinmod

--- HDG/Analytical_computations.m
+++ HDG/Analytical_computations.m
@@ -1,39 +1,43 @@
 clearvars
-syms x y k a b lambda
+syms x y k a b gamma
 
 %Analytical symbolic expression
-%u = sinh(a*sin(k*pi*x)+b*cos(pi*(x+lambda*y)));
+%u = sinh(a*sin(k*pi*x)+b*cos(pi*(x+gamma*y)));
 
-%u = k*log((x+a)^2)+cos(b*(y-lambda)^3); %agus
-u = cos(a*pi*(k*(x^2-b)))*sinh(-lambda*y); % eugenio
+%u = k*log((x+a)^2)+cos(b*(y-gamma)^3); %agus
+u = cos(a*pi*(k*(x^2-b)))*sinh(-gamma*y); % eugenio
+um= matlabFunction(u);
+latexum = latex(u);
 
 %Derived expressions
 delta_u = gradient(u, [x, y]); 
-s = - k*divergence(delta_u,[x, y]); %source
-s_F= matlabFunction(s);
+%Source Term
+s = - k*divergence(delta_u,[x, y]); 
+sm= matlabFunction(s);
+latexsm = latex(s);
 
 %Function u ---> Dirichlet 
-FUNC_u = matlabFunction(u);
-u_D = FUNC_u(1.2,0.75,5,3,x,y); %(a,b,k,lambda,x,y)
+u_D = um(a,b,k,gamma,x,y);
 
-x_vec0 = linspace(0,1,200);
-y_vec0 = linspace(0,1,200);
+
+%Plot of the function in 3D
+x_vec0 = linspace(0,1,100);
+y_vec0 = linspace(0,1,100);
 
 [x_vec, y_vec] = meshgrid(x_vec0,y_vec0);
 
-z_vec = FUNC_u(4,0.3,2.5,1.1,x_vec,y_vec);
+z_vec = um(4,0.3,2.5,1.1,x_vec,y_vec);
 
 surf(x_vec,y_vec,z_vec);
 
 
-%Neumann (y=0)
-n_N = [0 -1]; %check normal vector according to boundary
-FUNC_t = matlabFunction(dot(k*delta_u,n_N));
+%Neumann (x=1)
+n_N = [1 0]; %check normal vector according to boundary
+tm = matlabFunction(dot(k*delta_u,n_N));
+latextm= latex(dot(k*delta_u,n_N));
 
-
-%Robin (x=1)
-n_R = [1 0];
-FUNC_g = matlabFunction(simplify(dot(k*delta_u,n_R)+lambda*u))
-g = FUNC_g(1.2,0.75,5,3,x,y);
-
-%LaTeX_Expr = latex(u_D)
+%Robin (y=0)
+n_R = [0 -1];
+FUNC_g = matlabFunction(simplify(dot(k*delta_u,n_R)+gamma*u));
+gm = FUNC_g(a,b,k,gamma,x,y);
+latexgm = latex(simplify(dot(k*delta_u,n_R)+gamma*u));
